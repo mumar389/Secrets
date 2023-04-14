0b1cbfe7aba2894235f634416733365f06afb95b
Md Umar <mohammadumar389@gmail.com>
2023-04-14 15:29:44 +0530

    handled app

--- client/src/url.js
+++ client/src/url.js
@@ -1,4 +1,4 @@
-const base_url = "https://secrets-mrop.onrender.com/";
+const base_url = "https://secrets-mrop.onrender.com";
 const back_url = "http://localhost:5620";
 
 export { base_url, back_url };
