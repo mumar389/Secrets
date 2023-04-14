5b6087d60ff5f052481c31ab05a9b9d1ee70828b
Md Umar <mohammadumar389@gmail.com>
2023-04-14 15:25:47 +0530

    handled google login

--- client/src/url.js
+++ client/src/url.js
@@ -1,5 +1,8 @@
-const base_url =
-  "https://secrets-web.vercel.app/" || "https://secrets-mrop.onrender.com/";
+const base_url = "https://secrets-mrop.onrender.com/";
 const back_url = "http://localhost:5620";
 
 export { base_url, back_url };
+
+/*
+"https://secrets-web.vercel.app/" 
+*/
