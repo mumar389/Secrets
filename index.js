6d8a7e104c28e6d10bcb623f6847b4a3be7a68d9
Md Umar <mohammadumar389@gmail.com>
2023-04-15 12:29:45 +0530

    handled app

--- index.js
+++ index.js
@@ -34,9 +34,11 @@ if (process.env.MODE == "production") {
   app.use(express.static("./client/build"));
 
   //express will serve up the index.html file if routes doesnot match-:
-
+  app.get("/", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
   app.get("*", (req, res) => {
-    console.log("Inside me");
+    // console.log("Inside me");
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
