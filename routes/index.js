a6aaa2b2f2baa4db3aa3f2eb021c143a2c2d85dd
Md Umar <mohammadumar389@gmail.com>
2023-04-15 13:32:14 +0530

    handled error

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const router=express.Router();
 const homeControl=require('../controller/home_controller');
 
 //for production mode only
-router.get('/',homeControl.getHome);
+// router.get('/',homeControl.getHome);
 router.use('/api',require('./api/index'))
 
 
