9cb30bc25e8e6f33ff6726029e2099703bb35ffd
Md Umar <mohammadumar389@gmail.com>
2023-04-15 12:04:00 +0530

    handled app

--- controller/home_controller.js
+++ controller/home_controller.js
@@ -1,5 +1,5 @@
 //for production mode only
 const path = require("path");
 module.exports.getHome = async (req, res) => {
-  return res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  return res.sendFile(path.resolve(__dirname, '..',"client", "build", "index.html"));
 };
