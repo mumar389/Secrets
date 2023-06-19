8edc6da21352a9593bf096c61cea503e8d128168
Md Umar <mohammadumar389@gmail.com>
2023-06-19 21:11:49 +0530

    Final Commit

--- controller/userController.js
+++ controller/userController.js
@@ -113,7 +113,9 @@ module.exports.googleHome = async (req, res) => {
 };
 module.exports.logout = async (req, res) => {
   try {
-    req.logout(async (err, users) => {
+    res.clearCookie("jwt");
+    req.logout(async () => {
+      /*
       if (err) {
         console.log("Error",err);
         return res.status(501).json({
@@ -126,6 +128,10 @@ module.exports.logout = async (req, res) => {
           message: "Logout sucess",
         });
       }
+      */
+      return res.status(200).json({
+        message: "Logout sucess",
+      });
     });
   } catch (error) {
     console.log(error);
